5d0752155ff979a03db0e2b4865d12b742ea8f92
Mohsen <smmousavi.developer@gmail.com>
2018-07-26 00:35:00 +0430

    Commit by Mohsen: improvements: -Score updates automatically -The Question Show Fragment works properly -Final score is added to the last score of the user

--- app/src/main/java/com/example/smmousavi/maktab_hw10_quizrace/mvc/controller/fragments/QuizShowFragment.java
+++ app/src/main/java/com/example/smmousavi/maktab_hw10_quizrace/mvc/controller/fragments/QuizShowFragment.java
@@ -3,6 +3,8 @@ package com.example.smmousavi.maktab_hw10_quizrace.mvc.controller.fragments;
 
 import android.os.Bundle;
 import android.os.CountDownTimer;
+import android.os.Handler;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
@@ -15,7 +17,6 @@ import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import com.example.smmousavi.maktab_hw10_quizrace.R;
-import com.example.smmousavi.maktab_hw10_quizrace.mvc.controller.activities.QuizResultReviewPagerActivity;
 import com.example.smmousavi.maktab_hw10_quizrace.mvc.model.Answer;
 import com.example.smmousavi.maktab_hw10_quizrace.mvc.model.Question;
 import com.example.smmousavi.maktab_hw10_quizrace.mvc.model.Repository;
@@ -38,14 +39,15 @@ public class QuizShowFragment extends Fragment {
   private TextView mQuestionNumberTxt;
   private TextView mScoreTxt;
   private TextView mQuestionTextViewTxt;
-  private Button[] answerButtons;
+  private Button[] mAnswerButtons;
   private List<Question> mQuestionList;
   private List<Answer> mAnswers;
   private String mQuestionCategory;
   private String mQuestionDifficulty;
   private UUID mCurrentQuestionId;
-  private int mCurrentQuesionNumber;
   private Question mCurrentQuestion;
+  private int mCurrentQuesionNumber;
+  private long mCurrentScore;
   private User mCurrentUser;
 
 
@@ -58,7 +60,6 @@ public class QuizShowFragment extends Fragment {
   private static final long TIME_COUNT_MILLIES = 15000;
 
   private TimerStatus timerStatus = TimerStatus.STOPPED;
-  private TimerZero mCallback;
   private ProgressBar progressBarCircle;
   private TextView textViewTime;
   private CountDownTimer countDownTimer;
@@ -92,6 +93,7 @@ public class QuizShowFragment extends Fragment {
   @Override
   public void onCreate(@Nullable Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
+    setRetainInstance(true);
     mQuestionCategory = getArguments().getString(ARGS_QUESTION_CATEGORY);
     mQuestionDifficulty = getArguments().getString(ARGS_QUESTION_LEVEL);
     mQuestionList = Repository.getInstance(getActivity()).getQuestionsList(mQuestionCategory, mQuestionDifficulty);
@@ -101,18 +103,20 @@ public class QuizShowFragment extends Fragment {
 
 
   @Override
-  public View onCreateView(LayoutInflater inflater, ViewGroup container,
+  public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
                            Bundle savedInstanceState) {
     // Inflate the layout for this fragment
     View view = inflater.inflate(R.layout.fragment_quiz_show, container, false);
 
     findViews(view);
 
-    answerButtons = new Button[]{
+    mAnswerButtons = new Button[]{
       view.findViewById(R.id.answer_one),
       view.findViewById(R.id.answer_two),
       view.findViewById(R.id.answer_three),
       view.findViewById(R.id.answer_four)};
+
+    setOnAnswerButtonClickListener();
 
     mCategoryTxt.setText(getString(R.string.quiz_show_specification_category, mQuestionCategory));
     mDifficultyTxt.setText(getString(R.string.quiz_show_specification_level, mQuestionDifficulty));
@@ -131,6 +135,7 @@ public class QuizShowFragment extends Fragment {
     mQuestionTextViewTxt = view.findViewById(R.id.txt_question);
     progressBarCircle = view.findViewById(R.id.progress_bar_circle);
     textViewTime = view.findViewById(R.id.txt_time_counter);
+
   }// end of findViews()
 
 
@@ -138,44 +143,105 @@ public class QuizShowFragment extends Fragment {
     mCurrentQuestion = mQuestionList.get(mCurrentQuesionNumber);
     mCurrentQuestionId = mCurrentQuestion.getId();
     mAnswers = Repository.getInstance(getActivity()).getAnswersList(mCurrentQuestionId);
-    // this must be modified
-    mScoreTxt.setText(getString(R.string.quiz_show_specification_score, QuizResultReviewPagerActivity.scoreSum));
+    mScoreTxt.setText(getString(R.string.quiz_show_specification_score, mCurrentScore));
     mQuestionTextViewTxt.setText(mCurrentQuestion.getText());
-    for (int i = 0; i < answerButtons.length; i++) {
-      answerButtons[i].setText(mAnswers.get(i).getText());
-      answerButtons[i].setTag("" + mAnswers.get(i).isTrue());
+    mQuestionNumberTxt.setText(getString(R.string.quiz_show_specification_question_number, mCurrentQuesionNumber + 1));
+
+    for (int i = 0; i < mAnswerButtons.length; i++) {
+      mAnswerButtons[i].setText(mAnswers.get(i).getText());
+      mAnswerButtons[i].setTag("" + mAnswers.get(i).isTrue());
     }
   }// end of updateAfterUserAnswered()
 
 
-  private void setOnAnswerButtonsClickListener() {
+  private void setOnAnswerButtonClickListener() {
+    for (final Button button : mAnswerButtons) {
+      button.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+          boolean isTrueAnswer = Boolean.parseBoolean(button.getTag().toString());
+          if (isTrueAnswer) {
+            mCurrentScore += 30;
+            updateAfterUserAnswered();
 
+            deactivateButtons();
+
+            stopCountDownTimer();
+
+            rightAnswerAction();
+
+            final Handler handler = new Handler();
+            handler.postDelayed(new Runnable() {
+              @Override
+              public void run() {
+                //Do something after 1000ms
+                countDownTimer.onFinish();
+              }
+            }, 1000);
+
+          } else {
+            mCurrentScore -= 10;
+            updateAfterUserAnswered();
+
+            deactivateButtons();
+
+            stopCountDownTimer();
+
+            wrongAnswerAction();
+
+            final Handler handler = new Handler();
+            handler.postDelayed(new Runnable() {
+              @Override
+              public void run() {
+                countDownTimer.onFinish();
+              }
+            }, 1000);
+          }
+        }
+      });
+    }
   }// end of setOnAnswerButtonsClickListener()
+
+
+  private void wrongAnswerAction() {
+    Snackbar.make(getView(), "You Choosed Wrong Answer", Snackbar.LENGTH_SHORT).show();
+
+  }// end of wrongAnswerAction()
+
+
+  private void rightAnswerAction() {
+    Snackbar.make(getView(), "You Choosed Right Answer", Snackbar.LENGTH_SHORT).show();
+
+  }// end of rightAnswerAction()
+
+
+  private void finishQuiz() {
+    //temperory actioon, must be modified
+    Snackbar.make(getView(), "Quiz Finished", Snackbar.LENGTH_SHORT).show();
+    deactivateButtons();
+    stopCountDownTimer();
+    long totalScore = mCurrentUser.getTotalScore();
+    totalScore += mCurrentScore;
+    mCurrentUser.setTotalScore(totalScore);
+    Repository.getInstance(getActivity()).updateUser(mCurrentUser);
+  }// end of finishQuiz();
+
+  private void deactivateButtons() {
+    for (Button button : mAnswerButtons) {
+      button.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+
+        }
+      });
+    }
+  }
 
   /*
    *
-   * Count Down Timer Methods
+   * Count Down Timer Impelemenation
    *
    */
-
-
-  private void resetTimer() {
-    stopCountDownTimer();
-    startCountDownTimer();
-  }
-
-  //method to start and stop count down timer
-  public void startOrStop() {
-    if (timerStatus == TimerStatus.STOPPED) {
-      setProgressBarValues();
-      timerStatus = TimerStatus.STARTED;
-      startCountDownTimer();
-
-    } else {
-      timerStatus = TimerStatus.STOPPED;
-      stopCountDownTimer();
-    }
-  }
 
   //method to start count down timer
   private void startCountDownTimer() {
@@ -191,28 +257,40 @@ public class QuizShowFragment extends Fragment {
       public void onFinish() {
         textViewTime.setText(sTimeFormatter(TIME_COUNT_MILLIES));
         setProgressBarValues();
-        //Send message to change question
         timerStatus = TimerStatus.STOPPED;
 
         if (++mCurrentQuesionNumber < mQuestionList.size()) {
           resetTimer();
           updateAfterUserAnswered();
+          setOnAnswerButtonClickListener();
 
-        } else {
+        } else
           finishQuiz();
-
-        }
       }
 
     }.start();
-    countDownTimer.start();
   }
 
 
-  private void finishQuiz() {
-    //temperory actioon, must be modified
-    Snackbar.make(getView(), "Quiz Finished", Snackbar.LENGTH_SHORT).show();
+  private void resetTimer() {
+    stopCountDownTimer();
+    startCountDownTimer();
   }
+
+
+  //method to start and stop count down timer
+  public void startOrStop() {
+    if (timerStatus == TimerStatus.STOPPED) {
+      setProgressBarValues();
+      timerStatus = TimerStatus.STARTED;
+      startCountDownTimer();
+
+    } else {
+      timerStatus = TimerStatus.STOPPED;
+      stopCountDownTimer();
+    }
+  }
+
 
   //method to stop count down timer
   private void stopCountDownTimer() {
@@ -230,31 +308,5 @@ public class QuizShowFragment extends Fragment {
     return String.valueOf(milliSeconds / 1000);
   }
 
-
-  //Callback
-  public interface TimerZero {
-    public void changeQuestion();
-  }
-
-  @Override
-  public void onAttachFragment(Fragment childFragment) {
-    super.onAttachFragment(childFragment);
-
-    try {
-      mCallback = (TimerZero) childFragment;
-    } catch (ClassCastException e) {
-      throw new ClassCastException(childFragment.toString() + " must implement TimerZero");
-    }
-  }
-
-  public void send() {
-    mCallback.changeQuestion();
-  }
-
-  @Override
-  public void onDetach() {
-    mCallback = null;
-    super.onDetach();
-  }
 
 }
